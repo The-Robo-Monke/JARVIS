e0fbe23083c83adeca1e01afb01446163313e7ac
Nandu Ashok <cna777@outlook.com>
2021-12-08 09:50:16 +0530

    Create J.A.R.V.I.S.py

---
+++ J.A.R.V.I.S.py
@@ -0,0 +1,99 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import webbrowser
+import wikipedia
+import pyjokes
+import pyautogui
+import requests
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def input_query():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('tell me....')
+        recognizer.pause_threshold = 0.7
+        voice = recognizer.listen(source)
+        try:
+            query = recognizer.recognize_google(voice).lower()
+            print('this is the query that was made....', query)
+            return query
+        except Exception as ex:
+            print('An exception occurred', ex)
+
+
+def report_time():
+    current_time = datetime.datetime.now().strftime('%I:%M %p')
+    return current_time
+
+
+def speak_va(transcribed_query):
+    engine.say(transcribed_query)
+    engine.runAndWait()
+
+
+def make_request(url):
+  response = requests.get(url)
+  return response.text
+
+
+def activate_va():
+    user_query = input_query()
+    print('user query ....', user_query)
+    if 'time' in user_query:
+        current_time = report_time()
+        print(f"the current time is {current_time}")
+        speak_va(f"the current time is {current_time}")
+    
+    elif 'You up' in user_query:
+        speak_va("always at your call sir")
+
+    elif 'What is your name?' in user_query:
+        speak_va("I am Jarvis")
+
+    elif 'Who are you?' in user_query:
+        speak_va("I am Sir's Assistant")
+
+    elif 'sir' in user_query:
+        speak_va('He is my God')
+
+    elif 'jarvis' in user_query:
+        speak_va('What can I do for you sir?')
+
+    elif 'open website' in user_query:  
+        speak_va(
+            "Please type the name of the website that you want to open (specify the full url) \n")
+        website_name = input()
+        print(website_name)
+        webbrowser.get(
+            'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s').open(website_name)
+        speak_va(f"{website_name} opened.")
+
+    elif 'wikipedia' in user_query:
+        speak_va("Searching on Wikipedia")
+        user_query = user_query.replace('wikipedia', ' ')
+        result = wikipedia.summary(user_query, sentences=4)
+        print(result)
+        speak_va(result)
+
+    elif 'joke' in user_query:
+        random_joke = pyjokes.get_joke()
+        print(random_joke)
+        speak_va(random_joke) 
+
+    elif 'screenshot' in user_query:
+        image = pyautogui.screenshot()
+        image.save('screenshot.png')
+        speak_va('Screenshot taken.')
+
+    elif 'search' in user_query:
+        speak_va("What do you want me to search for (please type) ? ")
+        search_term = input()
+        search_url = f"https://www.google.com/search?q={search_term}"
+        webbrowser.open(search_url)
+        speak_va(f"here are the results for the search term: {search_term}")
+while True:
+    activate_va()
